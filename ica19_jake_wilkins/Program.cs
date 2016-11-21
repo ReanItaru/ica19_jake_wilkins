c926ced46faf1c45c42e00fa4f7fdf987fe0f019
ReanItaru <uratinear@gmail.com>
2016-11-20 17:50:07 -0700

    Add project files.

---
+++ ica19_jake_wilkins/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ica19_jake_wilkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
